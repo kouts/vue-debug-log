61108d50a31b886338dcc63c45415535b4fb2187
E6440\Kouts <koutsaftakis@gmail.com>
2020-10-26 21:25:54 +0200

    WIP - scaffold project

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    Debug Log
+  </div>
+</template>
+
+<script>
+import '@/scss/app.scss';
+
+export default {
+  //
+};
+</script>
---
+++ vue.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  lintOnSave: 'warning',
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = 'ProjectName';
+      args[0].meta = { description: 'A single page application created using Vue.js' };
+      return args;
+    });
+  }
+};
